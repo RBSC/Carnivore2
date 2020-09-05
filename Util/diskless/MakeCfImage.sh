9f96cca022f18c6ebc10287b977befcb408ea11c
vldmr <vldmrrr@small_eb>
2020-09-04 22:48:35 -0500

    Updated diskless loader for 32KB boot menu

--- Util/diskless/MakeCfImage.sh
+++ Util/diskless/MakeCfImage.sh
@@ -2,8 +2,8 @@
 SDIR=$(dirname `readlink -f $0`)
 DIR=`readlink -f $SDIR/../..`
 IMAGE=CfImage.bin
-dd if=/dev/zero ibs=1k count=16 | tr "\000" "\377" >$IMAGE
-dd if=$DIR/BootBlock/BOOTCMFC.BIN of=$IMAGE conv=notrunc
-cat $DIR/ROMs/BIDECMFC.BIN >> $IMAGE
-cat $DIR/ROMs/FMPCCMFC.BIN >> $IMAGE
+dd if=/dev/zero ibs=1k count=32 | tr "\000" "\377" >$IMAGE
+dd if=$DIR/BootMenu/BOOTCMFC.BIN of=$IMAGE conv=notrunc
+cat $DIR/BIOSes/BIDECMFC.BIN >> $IMAGE
+cat $DIR/BIOSes/FMPCCMFC.BIN >> $IMAGE
 echo Created $IMAGE
